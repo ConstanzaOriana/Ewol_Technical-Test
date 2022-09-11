91ac22842d6a1a97197024e44586cc6cad99d826
Constanza Oriana <cotyoriana95@gmail.com>
2022-09-10 22:36:55 -0300

    Fix_DOM

--- app.js
+++ app.js
@@ -1,71 +1,87 @@
+let minutes = 00;
+let seconds = 00;
+let tens = 00;
+let outputSeconds = document.getElementById("seconds");
+let outputTens = document.getElementById("tens");
+let buttonStart = document.getElementById("btn-start");
+let buttonStop = document.getElementById("btn-stop");
+let buttonReset = document.getElementById("btn-reset");
+let Interval;
 
-    var seconds = 00;
-    var tens = 00;
-    var OutputSeconds = document.getElementById("second");
-    var OutputTens = document.getElementById("tens");
-    var buttonStart = document.getElementById("btn-start");
-    var buttonStop = document.getElementById("btn-stop");
-    var buttonReset = document.getElementById("btn-reset");
-    var Interval;
+const botones = document.querySelectorAll('.boton');
 
-    counter = 0;
+const mainEvaluator = (signal="increase") => {
+if(signal === "increase"){
+    tens++;
+}
+else if(signal === "decrease"){
+    tens--;
+}
 
-    const botones = document.querySelectorAll('.boton');
+if(tens <= 9){
+    outputTens.innerHTML = "0" + tens;
+}
+if(tens > 9){
+    outputTens.innerHTML = tens;
+}
+if(tens > 99){
+    seconds++;
+    outputSeconds.innerHTML = "0" + seconds;
+    tens = 0;
+    outputTens.innerHTML = "0" + 0;
+}
+if(seconds > 9){
+    outputSeconds.innerHTML = seconds;
+}
 
-    botones.forEach(boton => {
-        boton.addEventListener('click', function(e) {
 
-            const estilos = e.currentTarget.classList;
-        
-            if(estilos.contains('disminuir')) {
-              counter--;
-            }
-            else if(estilos.contains('aumentar')) {
-              counter++;
-            }
-            else {
-              counter = 0;
-            }
-              valor.textContent = contador;
+if(tens < 0){
+    aux = tens;
+    aux -= aux * 2;
+    if(tens < -9){
+        outputTens.innerHTML = "-" + aux; 
+    }
+    /*if(tens < -99){
+        seconds--;
+        outputSeconds.innerHTML = "-" + seconds;
+        outputTens.innerHTML = tens;
+    }*/
+    else{
+        outputTens.innerHTML = "-0" + aux;
+    }
+}
+};
 
-  })
+
+botones.forEach(boton => {
+    boton.addEventListener('click', function(e) {
+
+        const estilos = e.currentTarget.classList; 
+    
+        if(estilos.contains('increase')) {
+            mainEvaluator();
+        }
+        else if(estilos.contains('decrease')) {
+            mainEvaluator("decrease");
+        }
+
+    })
 })
 
-    buttonStart.addEventListener('click', () => {
-        clearInterval(Interval);
-        Interval = setInterval(startTimer, 10);  // millisecond 10 = 0.01 second
-    });
+buttonStart.addEventListener('click', () => {
+    clearInterval(Interval);
+    Interval = setInterval(mainEvaluator, 10);
+});
 
-    buttonStop.addEventListener('click', () => {
-        clearInterval(Interval);
-    });
+buttonStop.addEventListener('click', () => {
+    clearInterval(Interval);
+});
 
-    buttonReset.addEventListener('click', () => {
-        clearInterval(Interval);
-        tens = "00";
-        seconds = "00";
-        OutputSeconds.innerHTML = seconds;
-        OutputTens.innerHTML = tens;
-    });
+buttonReset.addEventListener('click', () => {
+    clearInterval(Interval);
+    tens = "00";
+    seconds = "00";
+    outputSeconds.innerHTML = seconds;
+    outputTens.innerHTML = tens;
+});
 
-    function startTimer(){
-        tens++;
-        if(tens <= 9){
-            OutputTens.innerHTML = "0" + tens;
-        }
-
-        if(tens > 9){
-            OutputTens.innerHTML = tens;
-        }
-
-        if(tens > 99){
-            seconds++;
-            OutputSeconds.innerHTML = "0" + seconds;
-            tens = 0;
-            OutputTens.innerHTML = "0" + 0;
-        }
-
-        if(seconds > 9){
-            OutputSeconds.innerHTML = seconds;
-        }
-    }
