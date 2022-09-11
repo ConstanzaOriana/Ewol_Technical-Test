af014d4c2f9cc546ac03c46902a7d209425a8ef0
Constanza Oriana <cotyoriana95@gmail.com>
2022-09-11 07:43:49 -0300

    Fix_Counter

--- app.js
+++ app.js
@@ -1,72 +1,158 @@
 let minutes = 00;
 let seconds = 00;
 let tens = 00;
+let outputMinutes = document.getElementById("minutes");
 let outputSeconds = document.getElementById("seconds");
 let outputTens = document.getElementById("tens");
+let buttonIncrease = document.getElementById("btn-increase");
+let buttonDecrease = document.getElementById("btn-decrease");
 let buttonStart = document.getElementById("btn-start");
 let buttonStop = document.getElementById("btn-stop");
 let buttonReset = document.getElementById("btn-reset");
 let Interval;
 
-const botones = document.querySelectorAll('.boton');
-
-const mainEvaluator = (signal="increase") => {
-if(signal === "increase"){
-    tens++;
-}
-else if(signal === "decrease"){
-    tens--;
-}
-
-if(tens <= 9){
-    outputTens.innerHTML = "0" + tens;
-}
-if(tens > 9){
-    outputTens.innerHTML = tens;
-}
-if(tens > 99){
-    seconds++;
-    outputSeconds.innerHTML = "0" + seconds;
-    tens = 0;
-    outputTens.innerHTML = "0" + 0;
-}
-if(seconds > 9){
-    outputSeconds.innerHTML = seconds;
-}
-
-
-if(tens < 0){
-    aux = tens;
+const toPositive = (timeUnit) => {
+    aux = timeUnit;
     aux -= aux * 2;
-    if(tens < -9){
-        outputTens.innerHTML = "-" + aux; 
-    }
-    /*if(tens < -99){
-        seconds--;
-        outputSeconds.innerHTML = "-" + seconds;
-        outputTens.innerHTML = tens;
-    }*/
-    else{
-        outputTens.innerHTML = "-0" + aux;
-    }
-}
+    return aux;
 };
 
+const mainEvaluator = (signal="increase") => {
 
-botones.forEach(boton => {
-    boton.addEventListener('click', function(e) {
+    if(signal === "increase"){
+        tens++;
+    }
+    else if(signal === "decrease"){
+        tens--;
+    }
 
-        const estilos = e.currentTarget.classList; 
-    
-        if(estilos.contains('increase')) {
-            mainEvaluator();
+    let prefix = "";
+    let prefix2 = "";
+    let tensValue = "";
+    let secondsValue = "";
+    let minutesValue = "";
+    let increase = true;
+
+    ///////////////////////////////////////////////////
+
+    if(tens > 0){
+
+        prefix = "0";
+        prefix2 = "";
+
+        tensValue = tens;
+        secondsValue = seconds;
+        minutesValue = minutes;
+
+    }
+    else if(tens < 0){
+
+        prefix = "-0";
+        prefix2 = "-";
+
+        tensValue = toPositive(tens);
+        //secondsValue = toPositive(seconds);
+        //minutesValue = toPositive(minutes);
+
+    }
+    else if(tens === 0){
+        outputTens.innerHTML = "0" + tens;
+    }
+
+    ///////////////////////////////////////////////////
+
+    if(tensValue <= 9){
+        outputTens.innerHTML = prefix + tensValue;
+        console.log("A");
+    }
+    if(tensValue > 9){
+        outputTens.innerHTML = prefix2 + tensValue;
+        console.log("B");
+    }
+    if(tensValue > 99){
+        tens = 00;
+        outputTens.innerHTML = "0" + 0;
+        if(increase){
+            seconds++;
+            secondsValue = seconds;
         }
-        else if(estilos.contains('decrease')) {
-            mainEvaluator("decrease");
+        else{
+            seconds--;
+            secondsValue = toPositive(seconds);
+        }
+        outputSeconds.innerHTML = prefix + secondsValue;
+        console.log("C");
+    }
+    if(secondsValue > 9){
+        outputSeconds.innerHTML = prefix2 + secondsValue;
+        console.log("D");
+    }
+    if(secondsValue > 59){
+        seconds = 00;
+        outputSeconds.innerHTML = "0" + 0;
+        if(increase){
+            minutes++;
+            minutesValue = minutes;
+        }
+        else{
+            minutes--;
+            minutesValue = toPositive(minutes);
+        }
+        outputMinutes.innerHTML = prefix + minutesValue;
+        console.log("E");
+    }
+    if(minutesValue > 9){
+        outputMinutes.innerHTML = prefix2 + minutesValue;
+        console.log("F");
+    }
+    if(minutesValue > 59){
+        tens = 00;
+        outputTens.innerHTML = "0" + 0;
+        seconds = 00;
+        outputSeconds.innerHTML = "0" + 0;
+        minutes = 00;
+        outputMinutes.innerHTML = "0" + 0;
+        console.log("G");
+    }
+        
+    /*if(tens < 0){
+
+        prefix = "-0";
+        prefix2 = "-";
+
+        tensPositive = toPositive(tens);
+        secondsPositive = toPositive(seconds);
+        minutesPositive = toPositive(minutes);
+
+        if(tensPositive <= 9){
+            outputTens.innerHTML = prefix + tensPositive;
+        }
+        if(tensPositive > 9){
+            outputTens.innerHTML = prefix2 + tensPositive;
+        }
+        if(tensPositive > 99){
+            tens = 00;
+            outputTens.innerHTML = tens;
+            seconds--;
+            outputSeconds.innerHTML = prefix + secondsPositive;
         }
 
-    })
-})
+
+    }
+
+    if(tens === 0){
+        outputTens.innerHTML = "0" + tens;
+    }*/
+
+};
+
+buttonIncrease.addEventListener('click', () => {
+    mainEvaluator();
+});
+
+buttonDecrease.addEventListener('click', () => {
+    mainEvaluator("decrease");
+});
 
 buttonStart.addEventListener('click', () => {
     clearInterval(Interval);
@@ -81,6 +167,8 @@ buttonReset.addEventListener('click', () => {
     clearInterval(Interval);
     tens = "00";
     seconds = "00";
+    minutes = "00";
+    outputMinutes.innerHTML = minutes;
     outputSeconds.innerHTML = seconds;
     outputTens.innerHTML = tens;
 });
